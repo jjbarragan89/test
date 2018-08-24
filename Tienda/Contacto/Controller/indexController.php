c76f31800655b74b43b86e58f632734c7262c6cc
jbarragan <jbarragan@viajemos.com>
2018-08-24 08:54:02 -0500

    desde la rama json

--- Tienda/Contacto/Controller/indexController.php
+++ Tienda/Contacto/Controller/indexController.php
@@ -2,4 +2,13 @@
 	echo "hola mundo"; die(' murio');
 
 	echo "tercera linea";
+
+	echo "linea solidad";
+
+	echo "el origen un stash dentro de otro stash";
+
+	echo "desde rama json";
+
+	echo "desde rama json dos ";
+
  ?>
